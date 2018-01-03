8d24b11e7259aeace18d7fd2f872da19cb23141f
Mulx10 <mehulkumarnirala@iitkgp.ac.in>
2018-01-03 17:33:53 +0530

    kick

--- GoToPoint.py
+++ GoToPoint.py
@@ -3,6 +3,8 @@ import behavior
 import _GoToPoint
 import rospy
 from utils.math_functions import *
+from utils.config import *
+
 class GoToPoint(behavior.Behavior):
     """docstring for GoToPoint"""
     ##
@@ -12,12 +14,7 @@ class GoToPoint(behavior.Behavior):
         setup = 1 
         drive = 2
 
-    ##
-    ## @brief      Constructs the object.
-    ##
-    ## @param      self   The object
-    ## @param      point  The point
-    ##
+
     def __init__(self,continuous=False):
         # print "gtp"
         #GoToPoint.behavior.Behavior()
@@ -26,6 +23,9 @@ class GoToPoint(behavior.Behavior):
         super(GoToPoint,self).__init__()
         #self.state = state
 
+        self.name = "GoToPoint"
+
+        self.behavior_failed = False
 
         self.add_state(GoToPoint.State.setup,
             behavior.Behavior.State.running)
@@ -45,13 +45,13 @@ class GoToPoint(behavior.Behavior):
         self.add_transition(GoToPoint.State.drive,
             behavior.Behavior.State.completed,lambda:self.at_new_point(),'complete')
 
-    ##
-    ## @brief      { function_description }
-    ##
-    ## @param      self  The object
-    ##
-    ## @return     { description_of_the_return_value }
-    ##
+        self.add_transition(GoToPoint.State.setup,
+            behavior.Behavior.State.failed,lambda: self.behavior_failed,'failed')
+
+        self.add_transition(GoToPoint.State.drive,
+            behavior.Behavior.State.failed,lambda: self.behavior_failed,'failed')
+
+
     def add_point(self,point,orient):
         self.target_point = point
         self.theta = orient
@@ -62,14 +62,10 @@ class GoToPoint(behavior.Behavior):
     def target_present(self):
         return self.target_point is not None
 
-    ##
-    ## @brief      { function_description }
-    ##
-    ## @return     { description_of_the_return_value }
-    ##
+
     def at_new_point(self):
         #print (dist(self.target_point,self.new_point),210)
-        return dist(self.target_point,self.new_point) < 210.0
+        return dist(self.target_point,self.new_point) < DISTANCE_THRESH
 
         
     def on_enter_setup(self):
@@ -86,21 +82,19 @@ class GoToPoint(behavior.Behavior):
 
     def terminate(self):
         super().terminate()
-    ##
-    ## @brief      { function_description }
-    ##
-    ## @param      self   The object
-    ## @param      kub    The kub
-    ## @param      state  The state
-    ##
-    ## @return     { description_of_the_return_value }
-    ##
+
     def execute_drive(self):
         start_time = rospy.Time.now()
         start_time = 1.0*start_time.secs + 1.0*start_time.nsecs/pow(10,9)   
-        t = _GoToPoint.execute(start_time)
+        generatingfunction = _GoToPoint.execute(start_time,DISTANCE_THRESH)
+        for gf in generatingfunction:
+            self.kub,target_point = gf
+            # print self.behavior_failed
+
+            if vicinity_points(self.target_point,target_point):
+                self.behavior_failed = True
+                break
         self.new_point = self.kub.get_pos()
-        print self.new_point.x,self.new_point.y
         
 
     
